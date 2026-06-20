529a34d062f56efb76e204ccac871338e1ee88ca
AnhagaBallas <mickle1212@mail.ru>
2026-06-20 12:49:45 +0300

    add --format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .version('1.0.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'display help for command')
   .option('-f, --format [type]', 'output format')
-  .argument('<filepath1>', 'path to first file')
-  .argument('<filepath2>', 'path to second file');
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 program.parse();
