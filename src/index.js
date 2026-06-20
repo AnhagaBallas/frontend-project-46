2300cf2add3e1782fa1b922b04b89f69a4f2fcfb
AnhagaBallas <mickle1212@mail.ru>
2026-06-20 12:59:22 +0300

    implement flat JSON diff comparison

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 
@@ -23,8 +24,25 @@ const genDiff = (filepath1, filepath2) => {
   const data1 = parseData(readFile(filepath1), format1);
   const data2 = parseData(readFile(filepath2), format2);
 
-  console.log('File 1:', data1);
-  console.log('File 2:', data2);
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+
+  const lines = keys.flatMap((key) => {
+    if (!Object.hasOwn(data2, key)) {
+      return [`  - ${key}: ${data1[key]}`];
+    }
+    if (!Object.hasOwn(data1, key)) {
+      return [`  + ${key}: ${data2[key]}`];
+    }
+    if (data1[key] === data2[key]) {
+      return [`    ${key}: ${data1[key]}`];
+    }
+    return [
+      `  - ${key}: ${data1[key]}`,
+      `  + ${key}: ${data2[key]}`,
+    ];
+  });
+
+  return `{\n${lines.join('\n')}\n}`;
 };
 
 export default genDiff;
