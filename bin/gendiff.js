49ddaa538b53ad36e6ad11a9906a2dd00edb870a
AnhagaBallas <mickle1212@mail.ru>
2026-06-20 12:55:47 +0300

    add file reading and JSON parsing

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -10,6 +11,9 @@ program
   .helpOption('-h, --help', 'display help for command')
   .option('-f, --format [type]', 'output format')
   .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath2>')
+  .action((filepath1, filepath2, options) => {
+    genDiff(filepath1, filepath2, options.format);
+  });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+
+const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
+
+const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
+
+const parseData = (data, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    default:
+      throw new Error(`Unknown format: ${format}`);
+  }
+};
+
+const getFormat = (filepath) => path.extname(filepath).slice(1);
+
 const genDiff = (filepath1, filepath2) => {
-  // логика сравнения будет добавлена позже
-  return '';
+  const format1 = getFormat(filepath1);
+  const format2 = getFormat(filepath2);
+
+  const data1 = parseData(readFile(filepath1), format1);
+  const data2 = parseData(readFile(filepath2), format2);
+
+  console.log('File 1:', data1);
+  console.log('File 2:', data2);
 };
 
 export default genDiff;
