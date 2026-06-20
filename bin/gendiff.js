ea41ee19933c5a35ac04e75f484dd61de1f41a80
AnhagaBallas <mickle1212@mail.ru>
2026-06-20 09:42:11 +0300

    add gendiff cli with commander.js

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'display help for command')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+
+program.parse();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const genDiff = (filepath1, filepath2) => {
+  // логика сравнения будет добавлена позже
+  return '';
+};
+
+export default genDiff;
