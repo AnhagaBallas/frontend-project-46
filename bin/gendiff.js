dcd63fe0fb1cb0727c8650b8bf8a9d92bf0fde47
AnhagaBallas <mickle1212@mail.ru>
2026-06-20 12:47:08 +0300

    add --format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'display help for command')
+  .option('-f, --format [type]', 'output format')
   .argument('<filepath1>', 'path to first file')
-  .argument('<filepath2>', 'path to second file')
+  .argument('<filepath2>', 'path to second file');
 
 program.parse();
